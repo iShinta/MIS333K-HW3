ecc93896db39c8349411227d419d499ff7cbb993
iShinta <ho.minhtri.net@gmail.com>
2016-09-26 20:47:03 -0500

    First Commmit

---
+++ Ho_MinhTri_HW3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ho_MinhTri_HW3.MvcApplication" Language="C#" %>
---
+++ Ho_MinhTri_HW3/Migrations/201609270105434_InitialSetup.cs
@@ -0,0 +1,31 @@
+namespace Ho_MinhTri_HW3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialSetup : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        PhoneNumber = c.String(nullable: false),
+                        OKToText = c.Boolean(nullable: false),
+                        Major = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CustomerID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
